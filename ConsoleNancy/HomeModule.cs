df1bb3e5ef680f405b2686ac33d0dad22865fa58
Guilherme Froes <guilherme.froes@gmail.com>
2013-05-18 23:21:06 -0300

    listing some people

--- ConsoleNancy/HomeModule.cs
+++ ConsoleNancy/HomeModule.cs
@@ -1,13 +1,36 @@
-using System;
+using System.Collections;
+using System.Collections.Generic;
+using Nancy;
 
 namespace ConsoleNancy
 {
-	public class HomeModule : Nancy.NancyModule
+	public class HomeModule : NancyModule
 	{
 		public HomeModule ()
 		{
-			Get["/"] = _ => "bla bla";
+            Get["/"] = _ => Response.AsJson(GetPeopleList());
 		}
+
+	    private static IEnumerable<Person> GetPeopleList()
+	    {
+	        return new List<Person>
+	        {
+	            new Person("John Doe", 21),
+                new Person("John Doe", 21),
+	        };
+	    }
 	}
+
+    public class Person
+    {
+        public Person(string name, int age)
+        {
+            Age = age;
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+    }
 }
 
--- ConsoleNancy/Main.cs
+++ ConsoleNancy/Main.cs
@@ -8,14 +8,13 @@ namespace ConsoleNancy
 	{
 		public static void Main (string[] args)
 		{
-			var nancyHost = new NancyHost(new Uri("http://localhost:8080/"),
-			                              new Uri("http://127.0.0.1:8888/"),
-			                              new Uri("http://localhost:8889/"));
+			var nancyHost = new NancyHost(new Uri("http://localhost:80/"),
+			                              new Uri("http://127.0.0.1:80/"));
 
 			nancyHost.Start();
-
-			Console.WriteLine("nancy now listening - navigating to http://localhost:8888/. Press enter to stop.");
-			Process.Start("http://localhost:8888/");
+            
+			Console.WriteLine("nancy now listening - navigating to http://localhost:80/. Press enter to stop.");
+			Process.Start("http://localhost:80/");
 			Console.ReadKey();
 
 			nancyHost.Stop();
